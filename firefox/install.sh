762b39da14007655b3b821cc3b99f8a560495c64
Kevin Crane <kcrane@squareup.com>
2021-06-07 13:45:49 -0700

    Install Firefox userChrome and userContent locally (with some tweaks)

--- firefox/install.sh
+++ firefox/install.sh
@@ -2,20 +2,15 @@
 
 # Install Firefox
 
+CURRENT_DIR="${0:a:h}"
 FIREFOX_PROFILE=`ls -d ~/Library/Application\ Support/Firefox/Profiles/* | grep "default"`
 
 echo "=> Installing and configuring Firefox"
 brew ls --cask --versions firefox || brew install --cask firefox
 
 # Install Firefox Proton UI Fix
-wget -q -O firefox-proton-ui.zip https://github.com/black7375/Firefox-UI-Fix/archive/refs/heads/master.zip
-unzip -q firefox-proton-ui.zip
-cp -n Firefox-UI-Fix-master/user.js $FIREFOX_PROFILE
-mkdir -p ${FIREFOX_PROFILE}/chrome
-cp -n Firefox-UI-Fix-master/userChrome.css ${FIREFOX_PROFILE}/chrome
-cp -n Firefox-UI-Fix-master/userContent.css ${FIREFOX_PROFILE}/chrome
-cp -n -R Firefox-UI-Fix-master/icons ${FIREFOX_PROFILE}/chrome
-rm -rf Firefox-UI-Fix-master
-rm firefox-proton-ui.zip
+# Firefox UI Improvements from: https://github.com/black7375/Firefox-UI-Fix/
+echo "=> Copying userChrome files to $FIREFOX_PROFILE..."
+cp -R -i ${CURRENT_DIR}/profile/* $FIREFOX_PROFILE
 
 echo "=> Done installing Firefox"
