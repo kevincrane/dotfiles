42a942b66ba6cd21480b2661d9529208cfe97b4a
Kevin Crane <kcrane@squareup.com>
2021-07-08 10:12:55 -0700

    Symlink firefox profile files

--- firefox/install.sh
+++ firefox/install.sh
@@ -11,6 +11,12 @@ brew ls --cask --versions firefox || brew install --cask firefox
 # Install Firefox Proton UI Fix
 # Firefox UI Improvements from: https://github.com/black7375/Firefox-UI-Fix/
 echo "=> Copying userChrome files to $FIREFOX_PROFILE..."
-cp -R -i ${CURRENT_DIR}/profile/* $FIREFOX_PROFILE
+
+ln -sf profile/user.js $FIREFOX_PROFILE/user.js
+mkdir -p ${FIREFOX_PROFILE}/chrome
+ln -sf profile/chrome/userChrome.css $FIREFOX_PROFILE/chrome/userChrome.css
+ln -sf profile/chrome/userContent.css $FIREFOX_PROFILE/chrome/userContent.css
+mkdir -p ${FIREFOX_PROFILE}/chrome/icons
+ln -sf profile/chrome/icons/tab-bottom-corner.svg $FIREFOX_PROFILE/chrome/icons/tab-bottom-corner.svg
 
 echo "=> Done installing Firefox"
